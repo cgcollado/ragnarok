5d7143332cbb6aec06c1817f9f0fbd15e8e2ff91
cgcollado <clau18gc@gmail.com>
2018-04-03 01:46:49 +0200

    Initial commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject reference;
+    private Vector3 distance;
+	// Use this for initialization
+	void Start () {
+        distance = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		
+        distance = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * 2, Vector3.up) * distance;
+        transform.position = player.transform.position + distance;
+        transform.LookAt(player.transform.position);
+
+        //referencia para que los controles no se cambien segun la posicion de los ejes
+
+        Vector3 rotatitonCopy = new Vector3(0, transform.eulerAngles.y, 0);
+        reference.transform.eulerAngles = rotatitonCopy; 
+	}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    //Script creado a partir de un tutorial de Youtube del canal How Tuts
+
+    private Rigidbody player;
+    private float speed = 10f;
+    private float maxSpeed = 20f;
+    private bool isGround = true;
+    public GameObject reference;
+    private float jumpHeight = 7000f;
+
+	// Use this for initialization
+	void Start () {
+        player = GetComponent<Rigidbody>();
+
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveUp = Input.GetAxis("Jump");
+
+        if(player.velocity.magnitude > maxSpeed){
+            player.velocity = player.velocity.normalized * maxSpeed;
+        }
+
+        player.AddForce(moveVertical * reference.transform.forward * speed);
+        player.AddForce(moveHorizontal * reference.transform.right * speed);
+
+        if (Input.GetAxis("Jump")==1 && isGround)
+        {
+            player.AddForce(moveUp * reference.transform.up * jumpHeight);
+            //player.AddForce(0, jumpHeight, 0);
+            isGround = false;
+        }
+
+    }
+    void OnCollisionEnter (Collision collision)
+    {
+        Debug.Log("choque collider");
+        if (collision.collider.tag == "Ground")
+        {
+            isGround = true;
+            Debug.Log("IsGround");
+        }
+        else
+            isGround = false;
+    }
+}
